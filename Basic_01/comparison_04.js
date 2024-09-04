6d9ec0fd6a7a91cbdee736c115f0327f25290c02
Arham Bashir <120408066+Arha43@users.noreply.github.com>
2024-09-04 21:24:59 +0000

    6th

---
+++ Basic_01/comparison_04.js
@@ -0,0 +1,13 @@
+//console.log(2>1)
+//console.log(2<=1) etc
+//console.log(2!=1);
+
+//with two different type some times we dont get predicted result
+console.log("2">1);
+console.log("02">1)
+
+console.log(null>0)  // null is converted to undefined and undefined is not greater than 0
+console.log(null==0)  // null is converted to undefined and undefined is not equal to 0
+console.log(null>=0)
+
+//strict camparison === in this it will not change the type
