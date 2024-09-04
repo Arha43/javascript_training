096a9932816ac31c871c2138708a9fb2a742d265
Arham Bashir <120408066+Arha43@users.noreply.github.com>
2024-09-04 18:28:33 +0000

    data types and Conversion

---
+++ Basic_01/dataTypes_02.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we  are using nodejs, not browser
+
+//console.log(3+2); console.log("Arham here") // Code Readability (not good practice)
+
+//console.log(3
+//    +3)               //"Bad Practice"
+
+// --> Primitive Data types
+//number => 2 raise to power 53 
+//bigint
+//string =>" ",' '
+//boolean=>true,false
+//null => standalone value(Representation of empty)
+// undefined => value is not defined like let state; 
+//symbol
+
+//-->object
+
+
+console.log(typeof undefined)  //type undefined
+console.log(typeof null)       //type object
