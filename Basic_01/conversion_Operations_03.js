d10dba12c33c3832a96f6b9e4418323fe1dbf7bc
Arham Bashir <120408066+Arha43@users.noreply.github.com>
2024-09-04 20:12:43 +0000

    4th

--- Basic_01/conversion_Operations_03.js
+++ Basic_01/conversion_Operations_03.js
@@ -1,14 +1,24 @@
 //let num="243"
 //let num="243ABC"
 //let num=""
-let num=1
+//let num=1
 //console.log(typeof num);
 //console.log(typeof(num))
 
 // let valueInNumber=Number(num) //243ABC converted in Number(undefined)
-let valueInBoolean=Boolean(num) //243ABC converted in bool (true)
-console.log(typeof valueInBoolean) //"" converted in bool (false)
-console.log(valueInBoolean)
+//let valueInBoolean=Boolean(num) //243ABC converted in bool (true)
+//console.log(typeof valueInBoolean) //"" converted in bool (false)
+//console.log(valueInBoolean)
 
+// -------------- Operations --------------
 
+let value=2;
+let negativeValue=-value;
+
+console.log(negativeValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**4) //2 raised to the power 4
 
