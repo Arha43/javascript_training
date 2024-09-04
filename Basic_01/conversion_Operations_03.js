8e2e43d0279c65741bfc1784b5542a297668111f
Arham Bashir <120408066+Arha43@users.noreply.github.com>
2024-09-04 19:36:25 +0000

    4th

---
+++ Basic_01/conversion_Operations_03.js
@@ -0,0 +1,14 @@
+//let num="243"
+//let num="243ABC"
+//let num=""
+let num=1
+//console.log(typeof num);
+//console.log(typeof(num))
+
+// let valueInNumber=Number(num) //243ABC converted in Number(undefined)
+let valueInBoolean=Boolean(num) //243ABC converted in bool (true)
+console.log(typeof valueInBoolean) //"" converted in bool (false)
+console.log(valueInBoolean)
+
+
+
