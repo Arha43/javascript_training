2ba8426268e1a8ba4579030ecc329d850d0c94a4
Arham Bashir <120408066+Arha43@users.noreply.github.com>
2024-09-05 21:52:57 +0000

    summary final

--- Basic_01/summary_05.js
+++ Basic_01/summary_05.js
@@ -7,7 +7,7 @@
 
 const id=Symbol('123')
 const anotherId=Symbol('123')
-console.log(id===anotherId) //symbol will be different thats why false
+//console.log(id===anotherId) //symbol will be different thats why false
 
 const bigNumber=3333333333333333333222222222222n;
 
@@ -22,3 +22,25 @@ let obj={
     ali:"student",
     status:false
 }
+
+//-------------------------------Stack and Heap Memory in java Script ------------------
+
+// Primitive data types stores in stack and Non Primitive(refrence) in Heap
+
+//Primitive data type (using stack)
+
+let a=5;
+let b=a; //duplicates the value
+a=8;
+//console.log("Value of a : "+a)
+//console.log("Value of b : "+b)
+
+//Non Primitive (using heap memory)
+
+let obj1={name:"Arham"}
+let obj2=obj1;  //this is taking refrence of the memory
+
+obj2.name="Ali"  //and now changing the value every where in the refrence
+
+console.log(obj1.name)
+console.log(obj2.name)
